c603e771a29698023ca833b27e7315c2f3a98a4d
Federico Cappelli <68104865+FredHats@users.noreply.github.com>
2025-03-28 14:59:42 +0000

    ss

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-app.run(host='0.0.0.0', port=5000)
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
@@ -8,3 +8,6 @@ def hello_world():
 @app.route('/message')
 def message():
     return "ciao sono Fritz"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
